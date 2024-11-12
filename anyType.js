2e731b0a5bb0185ecde1b19e0b38f7066609ea41
Mayur Mandani <mmandani9@gmail.com>
2024-11-12 12:26:10 +0530

    Added the Typescript any Type file

---
+++ anyType.js
@@ -0,0 +1,8 @@
+// TypeScript has special types that may not refer to any specific type of data.
+// Type: any - any is a type that disables type checking and effectively allows all types to be used.
+var username = "true";
+username = "string";
+Math.round(username);
+console.log(username);
+/* any can be a useful way to get past errors since it disables type checking,
+but TypeScript will not be able provide type safety, and tools which rely on type data, such as auto completion, will not work. */ 
