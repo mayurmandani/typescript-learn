bfcd2c580d5c33a2d364ff304f1da63ef4bc8caa
Mayur Mandani <mmandani9@gmail.com>
2024-11-12 12:45:20 +0530

    Added the Typescript undefined and null Type file

---
+++ nullType.js
@@ -0,0 +1,9 @@
+/* Type: undefined & null
+undefined and null are types that refer to the JavaScript primitives undefined and null respectively.
+
+Note : These types don't have much use unless strictNullChecks is enabled in the tsconfig.json file.
+*/
+var y = undefined;
+var z = null;
+console.log(y);
+console.log(z);
---
+++ nullType.ts
@@ -0,0 +1,11 @@
+/* Type: undefined & null
+undefined and null are types that refer to the JavaScript primitives undefined and null respectively. 
+
+Note : These types don't have much use unless strictNullChecks is enabled in the tsconfig.json file.
+*/
+
+let y : undefined = undefined;
+let z : null = null;
+
+console.log(y);
+console.log(z);
