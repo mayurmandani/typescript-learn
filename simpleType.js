b4ad0530127a6a55d04e2a8021cc4a6141539f8b
Mayur Mandani <mmandani9@gmail.com>
2024-11-12 12:19:03 +0530

    Added the Typescript type file

---
+++ simpleType.js
@@ -0,0 +1,6 @@
+// Explicit type - assignment are easier to read and more intentional.
+var firstName = "Mayur";
+console.log(firstName);
+// Implicit - TypeScript will "guess" the type, based on the assigned value:
+var LastName = "Mandani";
+console.log(LastName);
---
+++ simpleType.ts
@@ -0,0 +1,10 @@
+// Explicit type - assignment are easier to read and more intentional.
+let firstName: string = "Mayur";
+
+console.log(firstName);
+
+
+// Implicit - TypeScript will "guess" the type, based on the assigned value:
+const LastName = "Mandani";
+
+console.log(LastName);
