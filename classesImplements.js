d280ae5b98633a730f7d3b06ca42256cfde417a9
Mayur Mandani <mmandani9@gmail.com>
2024-11-21 15:42:05 +0530

    Added the Typescript classes Inheritance Implements and Extends files

---
+++ classesImplements.js
@@ -0,0 +1,15 @@
+/* Inheritance: Implements
+Interfaces can be used to define the type a class must follow through the implements keyword.
+*/
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var myRect = new Rectangle(10, 20);
+console.log(myRect.getArea());
